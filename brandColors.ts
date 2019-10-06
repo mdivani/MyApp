c4c02879b315c6ca2528ddaa2eafe6e9f4afc405
George Mdivani <georgemdivani.gm@gmail.com>
2019-10-06 17:32:26 +0400

    bare react-native boilerplate

---
+++ brandColors.ts
@@ -0,0 +1,18 @@
+export const brandColors: any = {
+    "brand-color-1": "#ab6e42", // Brown/gold
+    "brand-color-1-alt": "#e9b279", // Gold
+    "brand-color-1-light": "#f2c386",
+    "brand-color-2": "#00738d", // Blue
+    "brand-color-2-light": "#54a6cd",
+    "brand-color-3": "#6F0D15", // Red
+    "brand-color-3-light": "#e94863",
+    "brand-color-4": "#a3a75b", // Green
+    "brand-color-4-light": "#d3da94",
+    "brand-color-5": "#ea742f", // Orange
+    "brand-color-5-light": "#f7aa5d",
+    "brand-color-6": "#743e71", // Purple
+    "brand-color-6-light": "#c794c2",
+    "brand-color-7": "#c994ca", // Light purple
+    "brand-color-8": "#763b71", // Dark purple
+    "brand-color-9": "#462343", // Darker purple
+};
